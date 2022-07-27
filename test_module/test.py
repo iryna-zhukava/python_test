caf2bd241ee2662472ade7919244fe14a079bca8
Iryna Zhukava <ireena.zhukova@gmail.com>
2022-07-27 13:23:59 +0300

    test

---
+++ test_module/test.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_new_upper(self):
+      self.assertEqual('foo'.upper(), 'FOO')
+  def test__new_isupper(self):
+      self.assertTrue('FOO'.isupper())
+      self.assertFalse('Foo'.isupper())
+
+  def test_new_split(self):
+      s = 'hello world'
+      self.assertEqual(s.split(), ['hello', 'world'])
+      # Проверим, что s.split не работает, если разделитель - не строка
+      with self.assertRaises(TypeError):
+          s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
